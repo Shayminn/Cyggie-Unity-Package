9a0612e04c6815e33f450f50a7a087379af1e326
Ryan Leung <rleung1022@gmail.com>
2023-02-16 23:33:44 -0500

    Added readonly attribute

---
+++ Cyggie-Unity-Package/Packages/main/Editor/Scripts/PropertyDrawers/ReadOnlyAttributeDrawer.cs
@@ -0,0 +1,20 @@
+﻿using Cyggie.Main.Runtime.Attributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace Cyggie.Main.Editor.PropertyDrawers
+{
+    /// <summary>
+    /// Property drawer for drawing the <see cref="ReadOnlyAttribute"/> as read-only
+    /// </summary>
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    internal class ReadOnlyAttributeDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = false;
+        }
+    }
+}
---
+++ Cyggie-Unity-Package/Packages/main/Runtime/Scripts/Attributes/ReadOnly/ReadOnlyAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Cyggie.Main.Runtime.Attributes
+{
+    /// <summary>
+    /// Custom Attribute for serializing a property in the inspector as read-only
+    /// </summary>
+    public class ReadOnlyAttribute : Attribute
+    {
+    }
+}
