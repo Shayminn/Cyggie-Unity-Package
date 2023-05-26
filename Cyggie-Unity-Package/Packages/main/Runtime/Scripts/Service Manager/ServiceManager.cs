17b3a0fb8b7bb29c49edf0b7554e917c8403f33d
Ryan Leung <rleung1022@gmail.com>
2023-05-26 01:57:53 -0400

    Fixed missing reference
    Re-added OnServicesInitialized but as an internal

--- Cyggie-Unity-Package/Packages/main/Runtime/Scripts/Service Manager/ServiceManager.cs
+++ Cyggie-Unity-Package/Packages/main/Runtime/Scripts/Service Manager/ServiceManager.cs
@@ -14,6 +14,12 @@ namespace Cyggie.Main.Runtime.ServicesNS
     public class ServiceManager : MonoBehaviour
     {
         /// <summary>
+        /// Action when all services have been created <br/>
+        /// This is called before <see cref="Service.Awake"/>
+        /// </summary>
+        internal static Action OnServicesInitialized = null;
+
+        /// <summary>
         /// Instance object of this class
         /// </summary>
         private static ServiceManager _instance = null;
@@ -83,6 +89,7 @@ namespace Cyggie.Main.Runtime.ServicesNS
             }
 
             Debug.Log($"[Service Manager] Initialized Services. Count: {_services.Count}.");
+            OnServicesInitialized?.Invoke();
         }
 
         #endregion
@@ -97,7 +104,7 @@ namespace Cyggie.Main.Runtime.ServicesNS
             // Check if Service Manager has been initialized
             if (_instance == null)
             {
-                Debug.LogError($"Failed to get a service, Service Manager has not yet been initialized.");
+                Debug.LogError($"Failed to get a service, Service Manager has not yet been initialized. Use {nameof(OnServicesInitialized)} or call Get in {nameof(Start)}.");
                 return default;
             }
 
