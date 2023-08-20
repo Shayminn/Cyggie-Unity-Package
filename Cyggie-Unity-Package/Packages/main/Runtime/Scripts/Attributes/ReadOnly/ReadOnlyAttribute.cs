a33198037ea23759bf781d2f36ff5cdf092499ac
Ryan Leung <49403271+Shayminn@users.noreply.github.com>
2023-08-19 21:25:50 -0400

    Fixed ReadOnly attribute

--- Cyggie-Unity-Package/Packages/main/Runtime/Scripts/Attributes/ReadOnly/ReadOnlyAttribute.cs
+++ Cyggie-Unity-Package/Packages/main/Runtime/Scripts/Attributes/ReadOnly/ReadOnlyAttribute.cs
@@ -1,11 +1,40 @@
 ﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Cyggie.Main.Runtime.Attributes
 {
     /// <summary>
     /// Custom Attribute for serializing a property in the inspector as read-only
     /// </summary>
-    public class ReadOnlyAttribute : Attribute
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class ReadOnlyAttribute : PropertyAttribute
     {
     }
+
+#if UNITY_EDITOR
+
+    //
+    // This needs to be in here because it needs to stay within the Cyggie.Main.Runtime assembly definition
+    //
+
+    /// <summary>
+    /// Property drawer for drawing the <see cref="ReadOnlyAttribute"/> as read-only
+    /// </summary>
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    internal class ReadOnlyAttributeDrawer : PropertyDrawer
+    {
+        /// <inheritdoc/>
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            Debug.Log("HELLO");
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = false;
+        }
+    }
+#endif
 }
