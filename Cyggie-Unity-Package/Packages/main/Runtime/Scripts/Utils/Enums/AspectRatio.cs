6cf4dae71fe14a9d0f96df3adff6061ccb282771
Ryan Leung <49403271+Shayminn@users.noreply.github.com>
2023-09-28 02:02:23 -0400

    Added AspectRatio
    - AspectRatio enum
    - AspectRatio helper
    - AspectRatio extensions

---
+++ Cyggie-Unity-Package/Packages/main/Runtime/Scripts/Utils/Enums/AspectRatio.cs
@@ -0,0 +1,56 @@
+using Cyggie.Main.Runtime.Utils.Helpers;
+
+namespace Cyggie.Main.Runtime.Utils.Enums
+{
+    /// <summary>
+    /// Enum that contains various of the existing aspect ratios <br/>
+    /// Use with <see cref="AspectRatioHelper"/> to retrieve an aspect ratio based on width and height
+    /// </summary>
+    public enum AspectRatio
+    {
+        /// <summary>
+        /// eg. 720x1280, 768x1366, 900x1600, 1080x1920, 1440x2560, 2160x3840, 2880x5120, 4320x7680
+        /// </summary>
+        _9x16,
+
+        /// <summary>
+        /// eg. 500x750, 720x1080, 1500x2250
+        /// </summary>
+        _2x3,
+
+        /// <summary>
+        /// eg. 1280x720, 1366x768, 1600x900, 1920x1080, 2560x1440, 3840x2160, 5120x2880, 7680x4320
+        /// </summary>
+        _16x9,
+
+        /// <summary>
+        /// eg. 1280x800, 1920x1200, 2560x1600
+        /// </summary>
+        _16x10,
+        
+        /// <summary>
+        /// eg. 270x180
+        /// </summary>
+        _3x2,
+
+        /// <summary>
+        /// eg. 1400x1050, 1440x1080, 1600x1200, 1920x1440, 2048x1536
+        /// </summary>
+        _4x3,
+
+        /// <summary>
+        /// eg. 1080x1350
+        /// </summary>
+        _5x4,
+
+        /// <summary>
+        /// eg. 2560x1080, 3440x1440, 3840x1600, 5120x2160
+        /// </summary>
+        _21x9,
+
+        /// <summary>
+        /// eg. 3840x1080, 5120x1440, 7680x2160
+        /// </summary>
+        _32x9
+    }
+}
---
+++ Cyggie-Unity-Package/Packages/main/Runtime/Scripts/Utils/Extensions/Enums/AspectRatioExtensions.cs
@@ -0,0 +1,43 @@
+using Cyggie.Main.Runtime.Utils.Enums;
+using UnityEngine;
+
+namespace Cyggie.Main.Runtime.Utils.Extensions
+{
+    /// <summary>
+    /// Extension class for <see cref="AspectRatio"/>
+    /// </summary>
+    public static class AspectRatioExtensions
+    {
+        /// <summary>
+        /// Get the Vector2 equivalent of an aspect ratio <br/>
+        /// where Vector2.x => width and Vector2.y = height
+        /// </summary>
+        /// <param name="ratio">Aspect ratio to convert</param>
+        /// <returns>Vector2 equivalent</returns>
+        public static Vector2 ToVector2(this AspectRatio ratio)
+        {
+            return ratio switch
+            {
+                AspectRatio._9x16 => new Vector2(9, 16),
+                AspectRatio._2x3 => new Vector2(2, 3),
+                AspectRatio._16x9 => new Vector2(16, 9),
+                AspectRatio._16x10 => new Vector2(16, 10),
+                AspectRatio._3x2 => new Vector2(3, 2),
+                AspectRatio._4x3 => new Vector2(4, 3),
+                AspectRatio._5x4 => new Vector2(5, 4),
+                AspectRatio._21x9 => new Vector2(21, 9),
+                AspectRatio._32x9 => new Vector2(32, 9),
+                _ => Vector2.zero,
+            };
+        }
+
+        /// <summary>
+        /// Get the Vector2 equivalent of an aspect ratio multiplied by <paramref name="multiplier"/> <br/>
+        /// where Vector2.x => width, Vector2.y => height
+        /// </summary>
+        /// <param name="ratio">Aspect ratio to convert</param>
+        /// <param name="multiplier">Multiplier to apply</param>
+        /// <returns>Vector2 equivalent</returns>
+        public static Vector2 ToVector2(this AspectRatio ratio, float multiplier) => ratio.ToVector2() * multiplier;
+    }
+}
