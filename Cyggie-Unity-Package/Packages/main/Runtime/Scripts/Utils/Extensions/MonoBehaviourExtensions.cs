d4d77a49f53b7763295418eedc5ecf7cb0364ee0
Ryan Leung <rleung1022@gmail.com>
2024-02-13 23:38:40 -0500

    Removed SerializationHelper
    Removed SerializationHelper
    - Moved to MonoBehaviourExtensions

--- Cyggie-Unity-Package/Packages/main/Runtime/Scripts/Utils/Extensions/MonoBehaviourExtensions.cs
+++ Cyggie-Unity-Package/Packages/main/Runtime/Scripts/Utils/Extensions/MonoBehaviourExtensions.cs
@@ -1,5 +1,8 @@
-﻿using Cyggie.Plugins.Utils;
+﻿using Cyggie.Main.Runtime.Serializations;
+using Cyggie.Plugins.Logs;
+using Cyggie.Plugins.Utils;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -23,6 +26,62 @@ namespace Cyggie.Main.Runtime.Utils.Extensions
         public static void DelayedAction(this MonoBehaviour mono, UnityAction action, float delay = cDefaultDelay)
         {
             mono.StartCoroutine(DelayedCoroutine(action, delay));
+        }
+
+        /// <summary>
+        /// Verify the Inspector Reference of one or many <paramref name="objs"/> <br/>
+        /// </summary>
+        /// <param name="mono">Mono object that holds the reference</param>
+        /// <param name="objs">Array of objects to check</param>
+        /// <returns>True if any <paramref name="objs"/> has a null reference</returns>
+        public static bool HasMissingReference(this MonoBehaviour mono, params UnityEngine.Object[] objs)
+        {
+            foreach (UnityEngine.Object obj in objs)
+            {
+                if (obj == null)
+                {
+                    Log.Error($"Object {obj.name} in {mono.name} has a missing reference, assign it in the Inspector.", nameof(MonoBehaviourExtensions), mono);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Verify the Inspector Reference of one or many <paramref name="objs"/> <br/>
+        /// </summary>
+        /// <param name="mono">Mono object that holds the reference</param>
+        /// <param name="objs">Array of objects to check</param>
+        /// <returns>True if any <paramref name="objs"/> has a null reference</returns>
+        public static bool HasMissingReference(this MonoBehaviour mono, params object[] objs)
+        {
+            foreach (object obj in objs)
+            {
+                if (obj == null)
+                {
+                    Log.Error($"Object in {mono.name} has a missing reference, assign it in the Inspector.", nameof(MonoBehaviourExtensions), mono);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Verify the Inspector References of all values in a <see cref="SerializedDictionary{TKey, TValue}"/>
+        /// </summary>
+        /// <param name="mono">Mono object that holds the reference</param>
+        /// <param name="dict">Dictionary's values to verify</param>
+        /// <returns>True if any of the <paramref name="dict"/>'s value has a null reference</returns>
+        public static bool HasMissingReference<TKey, TValue>(this MonoBehaviour mono, SerializedDictionary<TKey, TValue> dict)
+        {
+            foreach (KeyValuePair<TKey, TValue> kv in dict)
+            {
+                if (mono.HasMissingReference(kv.Value)) return true;
+            }
+
+            return false;
         }
 
         #endregion
